e5885b8efc6e49354d60a893f2fe48aeff3fecfb
Ibrohim Mubarok <273058784+ibrohimubabork@users.noreply.github.com>
2026-07-10 16:20:21 +0700

    Membuat repository getallbyuser

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 
+	"github.com/google/uuid"
 	"github.com/ibrohimubarok/task-tracker-api/internal/models"
 )
 
@@ -27,4 +28,36 @@ func (r *TaskRepository) Create(ctx context.Context, task *models.Tasks) error {
 	return r.DB.QueryRowContext(
 		ctx, query, task.ID, task.UserID, task.Title, task.Description, task.Status,
 	).Scan(&task.CreatedAt, &task.UpdatedAt)
+}
+
+func (r *TaskRepository) GetAllByUser(ctx context.Context, userID uuid.UUID) (tasks []models.Tasks, err error) {
+	query := `
+	SELECT id, user_id, title, description, status, created_at, updated_at
+	FROM tasks
+	WHERE user_id = $1
+	ORDER BY created_at DESC
+	`
+
+	rows, err := r.DB.QueryContext(
+		ctx, query, userID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var t models.Tasks
+		if err := rows.Scan(
+			&t.ID, &t.UserID, &t.Title, &t.Description, &t.Status, &t.CreatedAt, &t.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, t)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
 }
