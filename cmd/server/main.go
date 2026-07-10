e7354007d7a1d71c593a34aa40877aa8007ed037
Ibrohim Mubarok <273058784+ibrohimubabork@users.noreply.github.com>
2026-07-10 15:42:48 +0700

    Mengimplementasikan app ke server main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,7 +6,8 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/ibrohimubarok/task-tracker-api/internal/api"
+	"github.com/ibrohimubarok/task-tracker-api/internal/api/routes"
+	"github.com/ibrohimubarok/task-tracker-api/internal/app"
 	"github.com/ibrohimubarok/task-tracker-api/internal/config"
 	"github.com/ibrohimubarok/task-tracker-api/internal/database"
 )
@@ -22,9 +23,13 @@ func main() {
 	}
 	defer db.Close()
 	log.Println("Database connected successfully")
+
+	// Dependency Injection
+	app := app.New(db)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	api.Router(r)
+	routes.Route(r, app.Handlers)
 	log.Printf("Server running on port %s", cfg.Port)
 	if err := http.ListenAndServe(":"+cfg.Port, r); err != nil {
 		log.Fatal(err)
