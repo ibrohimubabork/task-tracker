78b3d618c1ba451716fd40da4cc051b703aaca33
Ibrohim Mubarok <273058784+ibrohimubabork@users.noreply.github.com>
2026-07-10 16:20:40 +0700

    Membuat handler getallbyuser

--- internal/api/handler/task_handler.go
+++ internal/api/handler/task_handler.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 	"github.com/ibrohimubarok/task-tracker-api/internal/models"
 	"github.com/ibrohimubarok/task-tracker-api/internal/response"
 	"github.com/ibrohimubarok/task-tracker-api/internal/service"
@@ -50,6 +52,41 @@ func (h *TaskHandler) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	response.JSON(w, http.StatusCreated, models.Response[models.Tasks]{
+		Status: "success",
+		Data:   tasks,
+	})
+}
+
+func (h *TaskHandler) GetAllByUser(w http.ResponseWriter, r *http.Request) {
+	userID := chi.URLParam(r, "user_id")
+	id, err := uuid.Parse(userID)
+	if err != nil {
+		response.JSON(w, http.StatusBadRequest, models.Response[any]{
+			Status: "error",
+			Errors: []models.ErrorResponse{
+				{
+					Code:    "INVALID_USER_ID",
+					Message: "invalid UUID format",
+				},
+			},
+		})
+		return
+	}
+	tasks, err := h.Service.GetAllByUser(r.Context(), id)
+	if err != nil {
+		response.JSON(w, http.StatusInternalServerError, models.Response[any]{
+			Status: "error",
+			Errors: []models.ErrorResponse{
+				{
+					Code:    "INTERNAL_SERVER_ERROR",
+					Message: "failed to get tasks",
+				},
+			},
+		})
+		return
+	}
+
+	response.JSON(w, http.StatusCreated, models.Response[[]models.Tasks]{
 		Status: "success",
 		Data:   tasks,
 	})
