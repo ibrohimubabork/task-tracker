9b4b6e20b8057e0cdd80ddadd031562e0587cf55
Ibrohim Mubarok <273058784+ibrohimubabork@users.noreply.github.com>
2026-07-10 14:25:28 +0700

    Buat models tabel tasks

---
+++ internal/models/tasks.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Tasks struct {
+	ID          uuid.UUIDs `json:"id"`
+	UserID      uuid.UUIDs `json:"user_id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description,omitempty"`
+	Status      string     `json:"status"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+}
