58dff7bc4d322e91fb9aa55b9c0e79b19482406b
Ibrohim Mubarok <273058784+ibrohimubabork@users.noreply.github.com>
2026-07-10 15:40:17 +0700

    Membuat service create tasks

---
+++ internal/service/task_service.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/ibrohimubarok/task-tracker-api/internal/models"
+	"github.com/ibrohimubarok/task-tracker-api/internal/repository"
+)
+
+type TaskService struct {
+	Repo *repository.TaskRepository
+}
+
+func NewTaskService(repo *repository.TaskRepository) *TaskService {
+	return &TaskService{
+		Repo: repo,
+	}
+}
+
+func (s *TaskService) Create(ctx context.Context, task *models.Tasks) error {
+	task.ID = uuid.New()
+
+	return s.Repo.Create(ctx, task)
+}
