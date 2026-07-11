500becb31fbcb1637506ecd3180f4d30457caa7f
Ibrohim Mubarok <273058784+ibrohimubabork@users.noreply.github.com>
2026-07-11 08:21:46 +0700

    Merubah strukture model users

--- internal/models/users.go
+++ internal/models/users.go
@@ -7,10 +7,20 @@ import (
 )
 
 type Users struct {
-	ID        uuid.UUID `json:"id"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	Role      string    `json:"role"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID           uuid.UUID `json:"id"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	Role         string    `json:"role"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type RegisterRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
