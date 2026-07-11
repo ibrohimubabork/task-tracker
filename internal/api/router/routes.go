21d4fa32c874fd6bd89b460d44bc292da9934d52
Ibrohim Mubarok <273058784+ibrohimubabork@users.noreply.github.com>
2026-07-11 08:26:59 +0700

    Masukkan userroutes ke route utama

--- internal/api/router/routes.go
+++ internal/api/router/routes.go
@@ -4,18 +4,20 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/ibrohimubarok/task-tracker-api/internal/api/handler"
-	"github.com/ibrohimubarok/task-tracker-api/internal/models"
-	"github.com/ibrohimubarok/task-tracker-api/internal/response"
+	"github.com/ibrohimubarok/task-tracker/internal/api/handler"
+	"github.com/ibrohimubarok/task-tracker/internal/models"
+	"github.com/ibrohimubarok/task-tracker/internal/response"
 )
 
 type Handlers struct {
 	Task *handler.TaskHandler
+	User *handler.UserHandler
 }
 
 func Route(r chi.Router, h *Handlers) {
 	r.Get("/", Root)
 	TaskRoutes(r, h.Task)
+	UserRoutes(r, h.User)
 }
 
 func Root(w http.ResponseWriter, r *http.Request) {
