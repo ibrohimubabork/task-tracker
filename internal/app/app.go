6a4103e9ac9a8052a4c477658da97711bbbf06c4
Ibrohim Mubarok <273058784+ibrohimubabork@users.noreply.github.com>
2026-07-11 08:26:34 +0700

    Injeksi user depedensi ke app

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,10 +3,10 @@ package app
 import (
 	"database/sql"
 
-	"github.com/ibrohimubarok/task-tracker-api/internal/api/handler"
-	"github.com/ibrohimubarok/task-tracker-api/internal/api/router"
-	"github.com/ibrohimubarok/task-tracker-api/internal/repository"
-	"github.com/ibrohimubarok/task-tracker-api/internal/service"
+	"github.com/ibrohimubarok/task-tracker/internal/api/handler"
+	"github.com/ibrohimubarok/task-tracker/internal/api/router"
+	"github.com/ibrohimubarok/task-tracker/internal/repository"
+	"github.com/ibrohimubarok/task-tracker/internal/service"
 )
 
 type App struct {
@@ -16,15 +16,19 @@ type App struct {
 func New(db *sql.DB) *App {
 	// Repository
 	taskRepo := repository.NewTaskRepository(db)
+	userRepo := repository.NewUserRepsitory(db)
 
 	// Service
 	taskService := service.NewTaskService(taskRepo)
+	userService := service.NewUserService(userRepo)
 
 	// Handler
 	taskHandler := handler.NewTaskHandler(taskService)
+	userHandler := handler.NewUserHandler(userService)
 	return &App{
 		Handlers: &router.Handlers{
 			Task: taskHandler,
+			User: userHandler,
 		},
 	}
 }
