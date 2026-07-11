28e4f8ea5a5202850da2e0cdd9b7e477e5d1f921
Ibrohim Mubarok <273058784+ibrohimubabork@users.noreply.github.com>
2026-07-11 08:22:10 +0700

    Merubah migration untuk membuat tabel users

--- internal/database/migrations/000001_create_users_table.up.sql
+++ internal/database/migrations/000001_create_users_table.up.sql
@@ -1,8 +1,9 @@
 CREATE TABLE users (
     id            UUID PRIMARY KEY,
     email         VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(20) not NULL DEFAULT 'users',
+    password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(20) not NULL DEFAULT 'user',
     created_at    TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at    TIMESTAMPTZ NOT NULL DEFAULT now()
+    updated_at    TIMESTAMPTZ NOT NULL DEFAULT now(),
+    CONSTRAINT chk_users_role CHECK (role IN ('user', 'admin'))
 );
