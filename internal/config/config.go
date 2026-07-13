229350148f2f0b94fc7fe7d3638843ddd9403451
Ibrohim Mubarok <273058784+ibrohimubarok@users.noreply.github.com>
2026-07-13 18:12:16 +0700

    Membuat config untuk membaca jwt secret

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,20 +4,27 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/go-chi/jwtauth/v5"
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
 	DatabaseURL string
 	Port        string
+	TokenAuth   *jwtauth.JWTAuth
 }
 
 func Load() (*Config, error) {
 	_ = godotenv.Load()
-
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return nil, fmt.Errorf("JWT_SECRET is required")
+	}
+	tokenAuth := jwtauth.New("HS256", []byte(jwtSecret), nil)
 	cfg := &Config{
 		DatabaseURL: os.Getenv("DATABASE_URL"),
 		Port:        os.Getenv("PORT"),
+		TokenAuth:   tokenAuth,
 	}
 
 	if cfg.DatabaseURL == "" {
