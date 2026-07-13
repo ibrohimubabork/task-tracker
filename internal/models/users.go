cc7e147d6ed6bef6c2b7de88995b104d9991b314
Ibrohim Mubarok <273058784+ibrohimubarok@users.noreply.github.com>
2026-07-13 18:14:51 +0700

    Buat login response

--- internal/models/users.go
+++ internal/models/users.go
@@ -24,3 +24,9 @@ type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type LoginResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int64  `json:"expires_in"`
+}
