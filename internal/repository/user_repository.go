77e405f4af18fe6928c91fa4259cf1ca76577869
Ibrohim Mubarok <273058784+ibrohimubarok@users.noreply.github.com>
2026-07-13 18:10:54 +0700

    Membuat getbyemail untuk fitur login

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"strings"
 
 	"github.com/ibrohimubarok/task-tracker/internal/models"
 )
@@ -27,4 +28,21 @@ func (r *UserRepository) Create(ctx context.Context, user models.Users) error {
 	return r.DB.QueryRowContext(
 		ctx, query, user.ID, user.Email, user.PasswordHash, user.Role,
 	).Scan(&user.CreatedAt, &user.UpdatedAt)
+}
+
+func (r *UserRepository) GetByEmail(ctx context.Context, email string) (models.Users, error) {
+	query := `
+		SELECT id, email, password_hash, role, created_at, updated_at
+		FROM users
+		WHERE LOWER(email) = LOWER($1)
+	`
+	var user models.Users
+	err := r.DB.QueryRowContext(
+		ctx, query, strings.TrimSpace(email),
+	).Scan(&user.ID, &user.Email, &user.PasswordHash, &user.Role, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		return models.Users{}, err
+	}
+
+	return user, nil
 }
