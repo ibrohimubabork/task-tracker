167283cbfc855a34d0487e57936d3ab3519f594e
Ibrohim Mubarok <273058784+ibrohimubarok@users.noreply.github.com>
2026-07-13 18:17:45 +0700

    Tambah tokenauth parameter

--- internal/api/router/routes.go
+++ internal/api/router/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
 	"github.com/ibrohimubarok/task-tracker/internal/api/handler"
 	"github.com/ibrohimubarok/task-tracker/internal/models"
 	"github.com/ibrohimubarok/task-tracker/internal/response"
@@ -14,9 +15,9 @@ type Handlers struct {
 	User *handler.UserHandler
 }
 
-func Route(r chi.Router, h *Handlers) {
+func Route(r chi.Router, h *Handlers, tokenAuth *jwtauth.JWTAuth) {
 	r.Get("/", Root)
-	TaskRoutes(r, h.Task)
+	TaskRoutes(r, h.Task, tokenAuth)
 	UserRoutes(r, h.User)
 }
 
