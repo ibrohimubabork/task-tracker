da23eef13aa5548698f1be46a8ed63beb7435270
Ibrohim Mubarok <273058784+ibrohimubarok@users.noreply.github.com>
2026-07-13 18:17:34 +0700

    Terapkan verifier dan authenticator ke taskroutes

--- internal/api/router/task_routes.go
+++ internal/api/router/task_routes.go
@@ -2,15 +2,22 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
 	"github.com/ibrohimubarok/task-tracker/internal/api/handler"
 )
 
-func TaskRoutes(r chi.Router, h *handler.TaskHandler) {
-	r.Route("/tasks", func(r chi.Router) {
-		r.Post("/", h.Create)
-		r.Get("/{id}", h.GetByID)
-		r.Get("/user/{user_id}", h.GetAllByUser)
-		r.Put("/{id}/{user_id}", h.Update)
-		r.Delete("/{id}/{user_id}", h.Delete)
+func TaskRoutes(r chi.Router, h *handler.TaskHandler, tokenAuth *jwtauth.JWTAuth) {
+
+	r.Group(func(r chi.Router) {
+		r.Use(jwtauth.Verifier(tokenAuth))
+		r.Use(jwtauth.Authenticator(tokenAuth))
+
+		r.Route("/tasks", func(r chi.Router) {
+			r.Post("/", h.Create)
+			r.Get("/{id}", h.GetByID)
+			r.Get("/user", h.GetAllByUser)
+			r.Put("/{id}", h.Update)
+			r.Delete("/{id}", h.Delete)
+		})
 	})
 }
